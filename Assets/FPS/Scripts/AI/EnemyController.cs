25810626c21991f5a485ddff16d2f835fb873f72
toneed <dydgns0729@gmail.com>
2024-11-14 18:00:13 +0900

    1. Enemy 패트롤 구현

--- Assets/FPS/Scripts/AI/EnemyController.cs
+++ Assets/FPS/Scripts/AI/EnemyController.cs
@@ -1,8 +1,29 @@
+using System.Collections.Generic;
 using Unity.FPS.Game;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
 
 namespace Unity.FPS.AI
 {
+    /// <summary>
+    /// 렌더러 데이터 : 메터리얼 정보 저장
+    /// </summary>
+    [System.Serializable]
+    public struct RendererIndexData
+    {
+        public Renderer renderer;
+        public int metarialIndex;
+
+        public RendererIndexData(Renderer _renderer, int index)
+        {
+            renderer = _renderer;
+            metarialIndex = index;
+        }
+    }
+    /// <summary>
+    /// Enemy를 관리하는 클래스
+    /// </summary>
     public class EnemyController : MonoBehaviour
     {
         #region Variables
@@ -11,19 +32,87 @@ namespace Unity.FPS.AI
         //death
         public GameObject deathVfxPrefab;
         public Transform deathVfxSpawnPostion;
+
+        //damage
+        public UnityAction Damaged;
+
+        //sfx
+        public AudioClip damageSfx;
+
+        //vfx
+        public Material bodyMaterial;           //데미지를 줄 메터리얼
+        [GradientUsage(true)]
+        public Gradient OnHitBodyGradient;      //데미지 효과를 컬러 그라디언트 효과로 표현
+        private List<RendererIndexData> bodyRenderer = new List<RendererIndexData>();   //bodyMaterial을 가지고 있는 렌더러 리스트
+        MaterialPropertyBlock bodyFlashMaterialPropertyBlock;
+
+        [SerializeField] private float flashOnHitDuration = 0.5f;
+        float lastTimeDamaged = float.NegativeInfinity;         //-의 최대값
+        bool wasDamagedThisFrame = false;                       //이번프레임에 데미지를 입었는지 체크해주는 변수
+
+        //Patrol
+        public NavMeshAgent Agent { get; private set; }
+        public PatrolPath PatrolPath { get; set; }
+        private int pathDestinationIndex;
+        private float pathReachingRadius = 1f;              //도착판정
         #endregion
 
         private void Start()
         {
             //참조
+            Agent = GetComponent<NavMeshAgent>();
+
             health = GetComponent<Health>();
             health.OnDamaged += OnDamaged;
             health.OnDie += OnDie;
+
+            //body Material을 가지고 있는 렌더러 정보 리스트 만들기
+            Renderer[] renderers = GetComponentsInChildren<Renderer>(true);
+            foreach (var renderer in renderers)
+            {
+                for (int i = 0; i < renderer.sharedMaterials.Length; i++)
+                {
+                    if (renderer.sharedMaterials[i] == bodyMaterial)
+                    {
+                        bodyRenderer.Add(new RendererIndexData(renderer, i));
+                    }
+                }
+            }
+
+            //
+            bodyFlashMaterialPropertyBlock = new MaterialPropertyBlock();
+        }
+
+        private void Update()
+        {
+            //데미지 효과
+            Color currentColor = OnHitBodyGradient.Evaluate((Time.time - lastTimeDamaged) / flashOnHitDuration);
+            bodyFlashMaterialPropertyBlock.SetColor("_EmissionColor", currentColor);
+            foreach (var data in bodyRenderer)
+            {
+                data.renderer.SetPropertyBlock(bodyFlashMaterialPropertyBlock, data.metarialIndex);
+            }
+
+            wasDamagedThisFrame = false;
         }
 
         private void OnDamaged(float damage, GameObject damageSource)
         {
+            if (damageSource && damageSource.GetComponent<EnemyController>() == null)
+            {
+                //등록된 함수 호출
+                Damaged?.Invoke();
 
+                //데미지를 준 시간
+                lastTimeDamaged = Time.time;
+
+                //Sfx
+                if (damageSfx && !wasDamagedThisFrame)
+                {
+                    AudioUtility.CreateSfx(damageSfx, this.transform.position, 0f);
+                }
+                wasDamagedThisFrame = true;
+            }
         }
 
         private void OnDie()
@@ -32,6 +121,71 @@ namespace Unity.FPS.AI
             Destroy(effectGo, 5f);
         }
 
+        //패트롤이 유효한지, 가능한지 체크
+        private bool IsPathVaild()
+        {
+            return PatrolPath && PatrolPath.wayPoints.Count > 0;
+        }
 
+        //가장 가까운 WayPoint 찾기
+        private void SetPathDestinationToClosestWayPoint()
+        {
+            if (IsPathVaild() == false)
+            {
+                pathDestinationIndex = 0;
+                return;
+            }
+
+            int closestWayPointIndex = 0;
+            for (int i = 0; i < PatrolPath.wayPoints.Count; i++)
+            {
+                float distance = PatrolPath.GetDistanceToWaypoint(transform.position, i);
+                float closestDistance = PatrolPath.GetDistanceToWaypoint(transform.position, closestWayPointIndex);
+                if (distance < closestDistance)
+                {
+                    closestWayPointIndex = i;
+                }
+            }
+            pathDestinationIndex = closestWayPointIndex;
+        }
+        //목표 지점의 위치 값 얻어오기
+        public Vector3 GetDestinationOnPath()
+        {
+            if (IsPathVaild() == false)
+            {
+                return this.transform.position;
+            }
+            return PatrolPath.GetPositionOfWayPoint(pathDestinationIndex);
+        }
+
+        //목표지점 설정 - Nav시스템 이용
+        public void SetNavDestination(Vector3 destination)
+        {
+            if (Agent)
+            {
+                Agent.SetDestination(destination);
+            }
+        }
+
+        //도착 판정 후 다음 목표지점 설정
+        public void UpdatePathDestination(bool inverseOrder = false)
+        {
+            if (IsPathVaild() == false) return;
+            //도착판정
+            float distance = (transform.position - GetDestinationOnPath()).magnitude;
+            if (distance <= pathReachingRadius)
+            {
+                pathDestinationIndex = inverseOrder ? pathDestinationIndex - 1 : pathDestinationIndex + 1;
+                if (pathDestinationIndex < 0)
+                {
+                    pathDestinationIndex += PatrolPath.wayPoints.Count;
+                }
+                if (pathDestinationIndex >= PatrolPath.wayPoints.Count)
+                {
+                    pathDestinationIndex -= PatrolPath.wayPoints.Count;
+                }
+
+            }
+        }
     }
 }
